7a0ed33babb0df238da24eba488d7c59dc54d88d
Ishaan Gupta <ishaangupta02@gmail.com>
2026-02-13 15:55:04 +0530

    Set up shadcn components and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
 
   return (
-    <>
-      <div>
-        Hello :)
+    <div className="min-h-screen bg-background flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <Button variant="default">Click Here</Button>
       </div>
-    </>
+    </div>
   )
 }
 
